a6d9d7f030efe169fb0255448773221e2b18175f
xieyao <1152188061@qq.com>
2020-07-11 12:13:02 +0800

    Event update , event message and event sound

--- src/main/java/com/Cassius/first/events/TestLevelUpEvent.java
+++ src/main/java/com/Cassius/first/events/TestLevelUpEvent.java
@@ -1,28 +1,34 @@
 package com.Cassius.first.events;
 
 import com.Cassius.first.FirstMod;
+import com.Cassius.first.init.SoundInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerXpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = FirstMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class TestJumpEvent {
+public class TestLevelUpEvent {
     @SubscribeEvent
     public static void testJumpEvent(PlayerXpEvent.LevelChange event){
-        FirstMod.LOGGER.info("Fired!!!!!");
+
         PlayerEntity playerEntity = event.getPlayer();
+        playerEntity.sendMessage(new TranslationTextComponent("Level Up!!"));
         World world = playerEntity.getEntityWorld();
+        BlockPos blockPos = new BlockPos(playerEntity);
+        Minecraft.getInstance().player.playSound(SoundInit.AMBIENT.get(),1.0f, 1.0f);
         Block diamondBlock = Blocks.DIAMOND_BLOCK;
         world.setBlockState(playerEntity.getPosition().down(), diamondBlock.getDefaultState());
         playerEntity.addPotionEffect(new EffectInstance(Effects.REGENERATION, 6000, 5));
         playerEntity.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 6000, 5));
         playerEntity.addPotionEffect(new EffectInstance(Effects.ABSORPTION, 6000, 5));
     }
-
 }
